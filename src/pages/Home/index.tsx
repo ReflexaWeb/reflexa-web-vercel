7731de56deecd87068138da99e22699ff7f44506
Ana Paula Moraes <ana@anawd.com>
2022-06-13 10:59:29 -0300

    Update Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,8 +3,8 @@ import { Helmet } from 'react-helmet';
 
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
-//import { Products } from "../../components/Products";
-//import { Sobre } from '../../components/Sobre';
+import { Products } from "../../components/Products";
+import { Sobre } from '../../components/Sobre';
 
 
 import { AvisoLanding } from '../../components/AvisoLanding'
@@ -17,7 +17,8 @@ export function Home() {
         <meta name="description" content="A Reflexa é referência no ramo de embalagens em Curitiba e região metropolitana. Estamos desde 1997 no mercado oferecendo produtos e atendimento de qualidade aos nossos clientes." />
       </Helmet>
       <Header/>
-      <AvisoLanding/>
+      <Products/>
+      <Sobre/>
       <Footer />
     </>
   );
