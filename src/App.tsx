115e46460a121bca72afa17a774af88748a14122
Ana Paula Moraes <ana@anawd.com>
2022-07-26 19:25:43 -0300

    Install GTM

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactGA from 'react-ga';
+import TagManager from 'react-gtm-module';  
 import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './routes';
@@ -7,6 +8,12 @@ import Routes from './routes';
 import GlobalStyle from './assets/styles/global'
 import { ApplicationProvider } from "./context/application";
 
+
+const tagManagerArgs = {
+    gtmId: 'AW-527629567'
+}
+
+TagManager.initialize(tagManagerArgs);
 ReactGA.initialize('UA-114594548-1');
 ReactGA.pageview(window.location.pathname + window.location.search);
 
