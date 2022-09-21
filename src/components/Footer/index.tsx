1dd975ae40d9db18cf834fcaa30295d5aa6dee7d
Ana Paula Moraes <ana@anawd.com>
2022-09-21 11:57:27 -0300

    Update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,8 +5,7 @@ import { Rodape } from './styles'
 export function Footer() {
   return (
     <Rodape>
-      <p>Segunda a sexta das 8:00 as 12:00 e das 13:30 as 18:00 - 
-         Sábado Não abrimos </p>
+      <p>Segunda a sexta das 8:00 as 18:00 - Sábado Não abrimos </p>
       <p>Whatsapp: (41) 99909 - 9627 | E-mail: vendas01@reflexa.com.br</p>
       <p>Reflexa Embalagens | Comércio de Embalagens em Curitiba | Todos os direitos reservados</p>
       <p>Atendemos CURITIBA  |  REGIAO METROPOLITANA MEDIANTE CONSULTA</p>
